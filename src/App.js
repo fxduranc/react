a25f44881c28512a2870fd0c8d62d52fec40b071
Francisco Duran <fx.duran.c@gmail.com>
2022-05-02 21:21:57 -0500

    agregar navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
+
+import NavBar from "./components/NavBar/NavBar";
+
 function App() {
   return (
 
     <div className="miApp">
-      <h1>Esta es mi primer aplicación en React.JS</h1>
-
+        <NavBar></NavBar>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-        
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+
+
+export default function NavBar() {
+    return (
+        <div class='container'>
+            <header className='header'>
+                <div class='container'>
+                    <div id="navigation-bar">
+                        <nav>
+                            <ul>
+                                <li><a href="#" id="logo">LOGO</a></li>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Portfolio</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <a href="#" id="menu-icon"></a>
+                            </ul>
+                        </nav>
+                        <br></br>
+                        <h1>Mi primera tienda en línea</h1>
+                    </div>
+                </div>
+            </header>
+        </div >
+    )
+
+
+}
